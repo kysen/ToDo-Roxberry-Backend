96eb8b312a86b480b11e9276ad889da0dd987d38
kysen <kysenj@gmail.com>
2019-06-25 12:40:21 -0600

    debugged

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CORS(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class SaveByDate (db.Model):
+class Date (db.Model):
     __tablename__="SaveListDaily"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
